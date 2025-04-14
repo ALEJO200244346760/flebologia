a128190735f55bfa83ffb17623ce267baceb82bc
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2025-04-14 16:39:32 -0300

    14-04

--- flebologia_frontend/src/axiosConfig.js
+++ flebologia_frontend/src/axiosConfig.js
@@ -1,55 +1,27 @@
+// src/axiosConfig.js
 import axios from 'axios';
 
-// Función para obtener el token del localStorage
 const getToken = () => {
-    const token = localStorage.getItem('token');
-    console.log('Token en localStorage:', token);  // Elimina esto en producción
-    return token;
+    return localStorage.getItem('token');
 };
 
-// Crear instancia de axios con configuración base
-const axiosInstance = axios.create({
-    baseURL: 'flebologia.railway.internal', // asegurate que no termine en /
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+// Crear una instancia de Axios con la configuración base
+const instance = axios.create({
+  baseURL: 'https://flebologia.railway.internal', // URL base del backend de Spring Boot
+});
 
-// Interceptor de solicitud para agregar el token de autorización
-axiosInstance.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response) {
-            if (error.response.status === 401) {
-                alert('Token no válido o ha expirado.');
-            }
-            return Promise.reject(error.response.data);
-        } else {
-            alert('Error al conectar con el servidor');
-            return Promise.reject(error);
+// Agregar un interceptor para las solicitudes
+instance.interceptors.request.use(
+    (config) => {
+        const token = getToken();
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
         }
-    }
-);
-
-// Interceptor de respuesta para manejar errores globalmente
-axiosInstance.interceptors.response.use(
-    (response) => {
-        return response;
+        return config;
     },
     (error) => {
-        // Si obtenemos un error 401 (Unauthorized)
-        if (error.response && error.response.status === 401) {
-            console.error('Token no válido o ha expirado.');
-
-            // Elimina el token inválido
-            localStorage.removeItem('token');
-
-            // Lanzar un error para manejarlo en el componente donde se hace la solicitud
-            return Promise.reject(error);
-        }
         return Promise.reject(error);
     }
 );
 
-// Exportar la instancia configurada de axios
-export default axiosInstance;
+export default instance;
