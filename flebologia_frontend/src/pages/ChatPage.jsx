32cfc423b1b577a333656ec0729273629a664fa1
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2025-04-23 16:44:01 -0300

    14-04

--- flebologia_frontend/src/pages/ChatPage.jsx
+++ flebologia_frontend/src/pages/ChatPage.jsx
@@ -1,7 +1,5 @@
-// src/pages/ChatPage.js
 import React from 'react';
 import ChatMessages from '../components/ChatMessages';
-import ChatForm from '../components/ChatForm';
 
 const ChatPage = () => {
   return (
@@ -9,12 +7,9 @@ const ChatPage = () => {
       <header className="bg-blue-600 text-white py-3 text-center">
         <h2 className="text-xl font-semibold">Chat con el Dr. Jorja</h2>
       </header>
-      <div className="flex-1 overflow-auto p-4">
-        <ChatMessages />
-      </div>
-      <div className="p-4 bg-white shadow-md">
-        <ChatForm />
-      </div>
+
+      {/* ✅ Este incluye ChatForm internamente */}
+      <ChatMessages />
     </div>
   );
 };
