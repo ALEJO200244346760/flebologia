05b10979e8f4bb6d843ccd86112c208641237003
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2025-04-25 16:56:23 -0300

    24-04

--- flebologia_frontend/src/pages/AdminDashboard.jsx
+++ flebologia_frontend/src/pages/AdminDashboard.jsx
@@ -15,14 +15,15 @@ const AdminDashboard = () => {
   return (
     <div className="pt-24 p-6 relative"> {/* padding top agregado para que no tape el Header */}
       {/* 👉 Botón "Usuarios" arriba a la derecha */}
+      
+
+      <h2 className="text-xl font-bold mb-6">Chats de Pacientes</h2>
       <button
         onClick={() => navigate('/admin/usuarios')}
         className="absolute right-6 top-6 bg-gray-800 text-white px-4 py-2 rounded shadow hover:bg-gray-700"
       >
         Usuarios
       </button>
-
-      <h2 className="text-xl font-bold mb-6">Chats de Pacientes</h2>
       <ul className="space-y-2">
         {usuariosConChat.map((user) => (
           <li key={user.id} className="p-4 bg-white shadow rounded flex justify-between items-center">
